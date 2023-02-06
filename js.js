46ea59f3217607c6a7494591c8a661003f7298e1
Dheeraj K Guptaa <119778059+DheerajGuptaa1@users.noreply.github.com>
2023-02-06 19:39:17 +0530

    Add files via upload

---
+++ js.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".auto-type",{
+    strings: [
+        "Web-Designer", "Front-end Developer", "UI-Experience Enhancer", "Aspiring ReactJS Developer", "Team-player", "Confident Communicator"],
+        typeSpeed:130,
+        backSpeed:110,
+        loop:true
+})
